a08bbfd718686d8677230a52801163cb3222ebd8
Dhrumit1677 <dhrumit.patel1677@gmail.com>
2022-10-23 15:55:38 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# COMP229_Assignment2" 
+"# COMP229_Assignment2" 
---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "Secret": 'SomeSecret',
+    "URI":"mongodb+srv://Dhrumit:JHbH7qfijQOaQLGP@mongodbserver.c0x7cql.mongodb.net/ContactList?retryWrites=true&w=majority"
+}
